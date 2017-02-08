73c7f0698012b7434b9ab7614559f5394eb7475f
Oksana Tunik <o.tunik@gmail.com>
2017-02-08 19:24:39 +0100

    Added effect as in tympanus

--- src/js/controllers/index_ctrl.js
+++ src/js/controllers/index_ctrl.js
@@ -12,8 +12,11 @@
       items: []
     };
 
+    vm.selectedProduct = null;
+
     vm.getTotal = getTotal;
     vm.addToCart = addToCart;
+    vm.toggleProductDetails = toggleProductDetails;
 
     activate();
 
@@ -21,6 +24,7 @@
       // TODO: fetch from API
       vm.products = [
         {
+          id: 1,
           title: 'Poducha #1',
           price: 9.99,
           description: 'Super poducha dziecięca w rybim kształcie',
@@ -28,6 +32,7 @@
           amount: 1
         },
         {
+          id: 2,
           title: 'Poducha #2',
           price: 9.99,
           description: 'Super poducha dziecięca w kształcie gwiazdki',
@@ -35,6 +40,7 @@
           amount: 1
         },
         {
+          id: 3,
           title: 'Poducha #3',
           price: 9.99,
           description: 'Super poducha-chmurka dziecięca',
@@ -42,6 +48,7 @@
           amount: 1
         },
         {
+          id: 4,
           title: 'Poducha #4',
           price: 9.99,
           description: 'Super poducha okrągła dziecięca',
@@ -49,6 +56,7 @@
           amount: 5
         },
         {
+          id: 5,
           title: 'Kocyk #1',
           price: 19.99,
           description: 'Super koc dziecięcy w stylistyce poduchi-chmurki Super koc dziecięcy w sty<br>listyce poduchi-chmurkiSuper koc dziecięcy w stylistyce poduchi-chmurkiSuper koc dziecięcy w stylistyce poduchi-chmurkiSuper koc dziecięcy w stylistyce poduchi-chmurki',
@@ -56,6 +64,7 @@
           amount: 1
         },
         {
+          id: 6,
           title: 'Poducha #5',
           price: 9.99,
           description: 'Super poducha okrągła dziecięca',
@@ -63,6 +72,7 @@
           amount: 5
         },
         {
+          id: 7,
           title: 'Poducha #4',
           price: 9.99,
           description: 'Super poducha okrągła dziecięca',
@@ -79,6 +89,14 @@
     function addToCart(product) {
       vm.cart.items.push(product);
       product.amount--;
+    }
+
+    function toggleProductDetails(product) {
+      if (vm.selectedProduct && product.id == vm.selectedProduct.id) {
+        vm.selectedProduct = null;
+      } else {
+        vm.selectedProduct = product;
+      }
     }
   }
 })();
